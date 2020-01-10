dfcfb7d1037b77a94d1ab3f4d4c8e84a90d37ac4
dfour001 <53315984+dfour001@users.noreply.github.com>
2020-01-09 19:05:14 -0500

    Create newsapi call and base html

---
+++ test.py
@@ -0,0 +1,61 @@
+from newsapi import NewsApiClient
+import json, os
+
+# newsapi-python documentation
+# https://github.com/mattlisiv/newsapi-python
+
+api = NewsApiClient(api_key = '8d33048321424617b34e3682c2bcff5f')
+
+# List of selected news sources, ordered by bias according to analysis by
+# allsides.com.  They are divided here for reference and will be collected
+# into a single list below.
+FarLeft = ["the-huffington-post",
+           "msnbc"]
+
+Left = ["cnn",
+        "nbc-news",
+        "politico",
+        "the-washington-post"]
+
+Center = ["associated-press",
+          "reuters",
+          "the-hill",
+          "usa-today"]
+
+Right = ["fox-news",
+         "the-wall-street-journal",
+         "the-washington-times",
+         "the-american-conservative"]
+
+FarRight = ["breitbart-news",
+            "national-review"]
+
+# Single list of selected sources
+sources = FarLeft + Left + Center + Right + FarRight
+
+# An empty dictionary containing all headlines that will be populated below
+headlinesDict = {}
+
+def download_top_headlines(source):
+    """ Download the headlines for the given source and save as a json file """
+
+    headlines = api.get_top_headlines(sources=source)
+    filename = f"{source}.json"
+    filepath = os.path.abspath(__file__) + "\\headlines\\" + filename
+    print(filepath)
+    with open(filepath, "w") as file:
+        file.write(json.dumps(headlines))
+
+
+# Download headlines for each source and append to headlinesDict
+for source in sources:
+    print(f"Downloading headlines from {source}")
+    headlines = api.get_top_headlines(sources=source)
+    headlinesDict[source] = headlines
+
+# Export headlinesDict as json file
+filename = "headlines.json"
+filepath = os.path.dirname(os.path.abspath(__file__)) + "\\headlines\\" + filename
+print(f"Saving headlines as headlines.json to {filepath}")
+with open(filepath, "w") as file:
+    file.write(json.dumps(headlinesDict))
